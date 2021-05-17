b788c5f189d00b8dea2a74237d14fba6ee824f0e
Andrew Borondia <andrew>
2021-05-17 18:26:36 -0500

    add getStopSchedules function

--- src/js/app.js
+++ src/js/app.js
@@ -5,57 +5,64 @@ const baseStopURL = `https://api.winnipegtransit.com/v3/stops.json?usage=long&ap
 class TransitSchedule {
   constructor() {
     this.searchResults = [];
-    this.stops = [];
+    this.streetStops = [];
+    this.stopSchedule = [];
   }
-  search = searchString => {
+
+  search = async searchString => {
     const searchURL = `${baseSearchURL}${searchString}`;
+    const searchResults = await transitSchedule.getData(searchURL);
 
-    return transitSchedule.getData(searchURL);
+    searchResults.streets.forEach(result => {
+      this.searchResults.push({ id: result.key, streetName: result.name });
+    })
+
+    return searchResults;
   }
 
-  getStops = street => {
+  getStops = async street => {
     // replace street key with target dataset key
-    const streetKey = street.streets[0].key;
-    const streetStops = transitSchedule.getData(`${baseStopURL}${streetKey}`);
-  
-    return streetStops;
+    const streetKey = this.searchResults[0].id;
+    const streetStops = await this.getData(`${baseStopURL}${streetKey}`);
+    streetStops.stops.forEach(stop => {
+      this.streetStops.push({ id: stop.key, name: stop.name, crossStreet: stop['cross-street'].name, direction: stop.direction })
+    })
+
+    this.getStopSchedules();
+
   }
 
   getData = async url => {
     const response = await fetch(url);
     const data = await response.json();
-  
+
     return data;
   }
-  
-  // const getStopTimes = (stops) => {
-  //   // const scheduledStops = 
-  //   // console.log(stops)
-  //   stops.forEach(stop => {
-  
-  //     console.log(new Date(stop['stop-schedule']['route-schedules'][0]['scheduled-stops'][0].times.arrival.estimated).toLocaleTimeString());
-  //   })
+
+  // getStopInformation = () => {
+
   // }
-  
-  getStopScheduleURL = stop => {
-    const scheduleURL = `https://api.winnipegtransit.com/v3/stops/${stop.key}/schedule.json?usage=long&api-key=${apiKey}`;
-  
+
+  getStopScheduleURL = stopId => {
+    const scheduleURL = `https://api.winnipegtransit.com/v3/stops/${stopId}/schedule.json?usage=long&api-key=${apiKey}`;
+
     return scheduleURL;
   }
-  
-  getStopSchedules = async streetStops => {
-    let scheduledStops = [];
-  
-    streetStops.stops.forEach(stop => {
-      const stopScheduleURL = transitSchedule.getStopScheduleURL(stop);
-  
-      scheduledStops.push(transitSchedule.getData(stopScheduleURL));
+
+  getStopSchedules = async () => {
+    this.streetStops.forEach(async streetStop => {
+      // streetStop.schedule = {};
+      const stopScheduleURL = this.getStopScheduleURL(streetStop.id);
+      const stops = await this.getData(stopScheduleURL);
+      streetStop.scheduledStops = stops;
+      // this.getStopInformation(stops, streetStop);
     })
-  
-    scheduledStops = await Promise.all(scheduledStops);
+
+    // scheduledStops = await Promise.all(scheduledStops);
     // const soonestStops = getStopTimes(scheduledStops);
-  
+
     // return soonestStops;
+    // return scheduledStops;
   }
 }
 
@@ -65,13 +72,14 @@ const transitSchedule = new TransitSchedule();
 
 transitSchedule.search('kinver')
   .then(searchResults => {
-    transitSchedule.searchResults = searchResults.streets;
+    // render search
     return transitSchedule.getStops(searchResults)
   })
   .then(stops => {
-    transitSchedule.stops = stops.stops;
-    console.log(transitSchedule.stops);
+    // transitSchedule.streetStops = stops.stops;
+    // return transitSchedule.getStopSchedules();
   })
+  .then(schedules => console.log(transitSchedule))
 // .then(()=> console.log(transitSchedule.searchResults) )
   // .then(data => buildHTMLLinks)
   // getStops on click
