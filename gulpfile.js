eb2e1aa581f36301570c6269747148573631cb21
Andrew Borondia <andrew>
2021-05-17 13:33:25 -0500

    add build tools to project

---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+const gulp = require('gulp');
+const browsersync = require('browser-sync');
+const { src, dest, series, parallel, watch } = require('gulp');
+const concat = require('gulp-concat');
+const crLfReplace = require('gulp-cr-lf-replace');
+const eslint = require('gulp-eslint');
+const htmlReplace = require('gulp-html-replace');
+const imagemin = require('gulp-imagemin');
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+
+function browserSync() {
+  return browsersync.init(
+    {
+      server: {
+        baseDir: "./dist"
+      },
+      port: 3000,
+    });
+}
+
+function htmlTask() {
+  return src('src/*.html')
+    .pipe(htmlReplace({
+      js: 'js/bundle.js',
+      css: 'styles/styles.css'
+    }))
+    .pipe(dest('dist'))
+    .pipe(browsersync.stream());
+}
+
+function scriptsTask() {
+  return src('src/js/*.js')
+    .pipe(crLfReplace())
+    .pipe(sourcemaps.init())
+    .pipe(concat('bundle.js'))
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(dest('dist/js'))
+    .pipe(browsersync.stream());
+}
+
+function stylesTask() {
+  return src('src/styles/*.css')
+    .pipe(autoprefixer())
+    .pipe(concat('styles.css'))
+    .pipe(dest('dist/styles'))
+    .pipe(browsersync.stream());
+}
+
+function watchFiles() {
+  watch('./src/js/*.js', scriptsTask);
+  watch('./src/', scriptsTask);
+}
+
+exports.html = htmlTask;
+exports.scripts = scriptsTask;
+exports.css = stylesTask;
+exports.watch = parallel(browserSync, watchFiles);
+exports.dev = series(parallel(htmlTask, scriptsTask, stylesTask), parallel(browserSync, watchFiles));
+exports.default = parallel(htmlTask, scriptsTask, stylesTask);
