7e3bc0d6637b97c5ed80c1862867773b7451df06
Andrew Borondia <andrew>
2021-05-17 11:14:43 -0500

    add api getData functionality

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-const apiKey = d7PLUCql1DUVNbas9tgX;
+const apiKey = 'd7PLUCql1DUVNbas9tgX';
+const baseSearchURL = `https://api.winnipegtransit.com/v3/streets.json?usage=long&api-key=${apiKey}&name=`;
+const baseStopURL = `https://api.winnipegtransit.com/v3/stops.json?usage=long&api-key=${apiKey}&street=`;
+// const baseStopScheduleURL = `https://api.winnipegtransit.com/v3/stops/${PUTAVARIABLEINHERE}/schedule.json?usage=long&api-key=${apiKey}`;
+
+const getData = async (url) => {
+  const response = await fetch(url);
+  const data = await response.json();
+
+  return data;
+}
+
+getData(baseSearchURL + 'henlow')
+  .then(data => console.log(data));
